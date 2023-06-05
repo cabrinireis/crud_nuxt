90f68100c67e30866a2d9467f0fe68c0678a78bf
cabrini <cabrinireis@gmail.com>
2023-06-05 12:13:28 -0300

    feat: add request crud

--- store/list_house/actions.js
+++ store/list_house/actions.js
@@ -14,15 +14,63 @@ export default {
       })
   },
 
-  async UPDATE_HOUSE({ commit }, params) {
+  async UPDATE_HOUSE({ commit, dispatch }, params) {
     commit('SET_LOADING', true)
     await this.$axios
       .put(
         `/api/admin/house_rules/${params.id}`,
         (params = { house_rules: params.params })
       )
+      .then(({ success }) => {
+        if (success) dispatch('LIST')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        commit('SET_LOADING', false)
+      })
+  },
+
+  async READ_HOUSE({ commit }, id) {
+    commit('SET_LOADING', true)
+    await this.$axios
+      .get(`/api/admin/house_rules/${id}`)
       .then(({ data }) => {
-        commit('SET_LIST', data)
+        if (data) commit('SET_HOUSE', data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        commit('SET_LOADING', false)
+      })
+  },
+
+  async CREATE_HOUSE({ commit, dispatch }, params) {
+    commit('SET_LOADING', true)
+    await this.$axios
+      .post(
+        `/api/admin/house_rules/`,
+        (params = { house_rules: params.params })
+      )
+      .then(({ data }) => {
+        if (data) dispatch('LIST')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        commit('SET_LOADING', false)
+      })
+  },
+
+  async DELETE_HOUSE({ commit, dispatch }, id) {
+    commit('SET_LOADING', true)
+    await this.$axios
+      .delete(`/api/admin/house_rules/${id}`)
+      .then(({ data }) => {
+        if (data) dispatch('LIST')
       })
       .catch((err) => {
         console.log(err)
