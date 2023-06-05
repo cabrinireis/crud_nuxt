d2833140d3e3919056ac73fb1705bf70467b4c9e
cabrini <cabrinireis@gmail.com>
2023-06-04 23:34:49 -0300

    feat: create component form

---
+++ components/FormHouse.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <b-form @submit="onSave">
+      <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          type="text"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Active:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.active"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Order:" label-for="input-3">
+        <b-form-input id="input-3" v-model="form.order" required></b-form-input>
+      </b-form-group>
+      <b-row>
+        <b-col cols="12" class="d-flex justify-content-end">
+          <b-button
+            type="reset"
+            class="mr-3"
+            variant="secondary"
+            @click="onClose"
+          >
+            Cancel
+          </b-button>
+          <b-button type="submit" variant="primary" @click="onSave"
+            >Save</b-button
+          >
+        </b-col>
+      </b-row>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        active: 0,
+        order: 0,
+      },
+    }
+  },
+  created() {
+    this.form = { ...this.data }
+  },
+  methods: {
+    onClose() {
+      this.$emit('closeModal')
+    },
+    onSave(event) {
+      event.preventDefault()
+    },
+  },
+}
+</script>
