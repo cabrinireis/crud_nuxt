8c59c985939c6b57065b255388fd0ae41577fe9c
cabrini <cabrinireis@gmail.com>
2023-06-05 16:25:57 -0300

    feat: config notification for error global

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,4 +4,25 @@ export default function ({ $axios, store, route }) {
       config.headers.common.Authorization = `Bearer ${store.state.session.token}`
     }
   })
+
+  $axios.onError((error) => {
+    if (error.ignore) {
+      return
+    }
+    if (!error.response.data.success) {
+      let message = 'Entity not found'
+      if (error.data?.errors?.length) {
+        message = error.data.errors.map((e) => e?.message).join('<br>')
+      }
+      store.commit(
+        'notification/SET_NOTIFICATION',
+        ['danger', 'Ops', message],
+        {
+          root: true,
+        }
+      )
+      throw error
+    }
+    throw error
+  })
 }
