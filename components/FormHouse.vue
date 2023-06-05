d5c1ce1f1017fb0b258616f941bee4f1804498b5
cabrini <cabrinireis@gmail.com>
2023-06-05 12:24:02 -0300

    feat: create page edit

--- components/FormHouse.vue
+++ components/FormHouse.vue
@@ -1,52 +1,63 @@
 <template>
-  <div>
-    <b-form @submit="onSave">
-      <b-form-group id="input-group-1" label="Name:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.name"
-          type="text"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-2" label="Active:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.active"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <!-- <b-form-group id="input-group-3" label="Order:" label-for="input-3">
-        <b-form-input id="input-3" v-model="form.order" required></b-form-input>
-      </b-form-group> -->
-      <b-row>
-        <b-col cols="12" class="d-flex justify-content-end">
-          <b-button
-            type="reset"
-            class="mr-3"
-            variant="secondary"
-            @click="onClose"
-          >
-            Cancel
-          </b-button>
-          <b-button type="submit" variant="primary" @click="onSave"
-            >Save</b-button
-          >
-        </b-col>
-      </b-row>
-    </b-form>
-  </div>
+  <b-container class="bv-example-row">
+    <b-row class="d-flex justify-content-center">
+      <b-col cols="8">
+        <b-form @submit="onSave">
+          <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="form.name"
+              type="text"
+              required
+              :readonly="readonly"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group id="input-group-2" label="Active:" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              v-model="form.active"
+              required
+              :readonly="readonly"
+            ></b-form-input>
+          </b-form-group>
+          <b-row>
+            <b-col v-if="readonly" cols="12" class="d-flex justify-content-end">
+              <b-button
+                type="reset"
+                class="mr-3"
+                variant="secondary"
+                @click="$router.push('/')"
+              >
+                Back
+              </b-button>
+            </b-col>
+            <b-col v-else cols="12" class="d-flex justify-content-end">
+              <b-button
+                type="reset"
+                class="mr-3"
+                variant="secondary"
+                @click="onClose"
+              >
+                Cancel
+              </b-button>
+              <b-button type="submit" variant="primary" @click="onSave"
+                >Save</b-button
+              >
+            </b-col>
+          </b-row>
+        </b-form>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 import { mapActions } from 'Vuex'
 export default {
   props: {
-    id: {
-      type: Number,
-      default: 0,
+    mode: {
+      type: String,
+      default: 'read',
     },
     data: {
       type: Object,
@@ -61,6 +72,11 @@ export default {
       },
     }
   },
+  computed: {
+    readonly() {
+      return this.mode === 'read'
+    },
+  },
   created() {
     this.form = { ...this.data }
   },
@@ -71,10 +87,10 @@ export default {
     onClose() {
       this.$emit('closeModal')
     },
-    onSave(event) {
+    async onSave(event) {
       event.preventDefault()
-      console.log(this.id)
-      this.update({ params: this.form, id: this.id })
+      await this.update({ params: this.form, id: this.$route.params.id })
+      this.$router.push('/')
     },
   },
 }
