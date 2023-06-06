4f16a56b60e708928ea5d9e2b392dc1e281bf731
cabrini <cabrinireis@gmail.com>
2023-06-06 14:56:01 -0300

    feat: add page error 404

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="error.statusCode === 404" class="d-flex justify-content-center">
+      <h1>Page not found</h1>
+    </div>
+    <div v-else class="d-flex justify-content-center">
+      <h1>An error occurred</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: ['error'], // you can set a custom layout for the error page
+}
+</script>
